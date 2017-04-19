8748105e9d7c5635719d8bc1139687caa3f65280
Cosmin <c0sm1n87@yahoo.co.uk>
2017-04-19 11:38:02 +0300

    missed conflict :D

--- class-epsilon-framework.php
+++ class-epsilon-framework.php
@@ -113,7 +113,7 @@ class Epsilon_Framework {
 			'theme'   => get_template_directory_uri(),
 			'ajaxurl' => admin_url( 'admin-ajax.php' )
 		) );
-		wp_enqueue_style( 'epsilon-styles', get_template_directory_uri() . '/inc/libraries/epsilon-framework/assets/css/style.css' );
+		wp_enqueue_style( 'epsilon-styles', get_template_directory_uri() . $this->path . '/epsilon-framework/assets/css/style.css' );
 
 	}
 
