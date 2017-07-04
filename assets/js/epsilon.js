a6e4f3fe56dbae69fa233dd41f0b00c5676f0be0
Cosmin <cristea.cosmin87@gmail.com>
2017-07-04 16:37:17 +0300

    Epsilon Control Toggle rewritten in JS Syntax

--- assets/js/epsilon.js
+++ assets/js/epsilon.js
@@ -444,6 +444,21 @@ EpsilonFramework.rangeSliders = {
 };
 
 /**
+ * WP Customizer Control Constructor
+ */
+wp.customize.controlConstructor[ 'epsilon-toggle' ] = wp.customize.Control.extend( {
+  ready: function() {
+    var control = this;
+
+    control.container.on( 'change', 'input.onoffswitch-checkbox',
+        function() {
+          control.setting.set( jQuery( this ).prop( 'checked' ) );
+        }
+    );
+  }
+} );
+
+/**
  * Typography functions
  *
  * @type {{_selectize: null, _linkedFonts: {}, init: EpsilonFramework.typography.init, _resetDefault: EpsilonFramework.typography._resetDefault, _parseJson:
--- controls/class-epsilon-control-toggle.php
+++ controls/class-epsilon-control-toggle.php
@@ -2,51 +2,80 @@
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
-if ( class_exists( 'WP_Customize_Control' ) ) {
+
+class Epsilon_Control_Toggle extends WP_Customize_Control {
 	/**
-	 * Multiple checkbox customize control class.
+	 * The type of customize control being rendered.
 	 *
 	 * @since  1.0.0
 	 * @access public
-	 *
+	 * @var    string
 	 */
-	class Epsilon_Control_Toggle extends WP_Customize_Control {
+	public $type = 'epsilon-toggle';
 
-		/**
-		 * The type of customize control being rendered.
-		 *
-		 * @since  1.0.0
-		 * @access public
-		 * @var    string
-		 */
-		public $type = 'epsilon-toggle';
-
-		/**
-		 * Displays the control content.
-		 *
-		 * @since  1.0.0
-		 * @access public
-		 * @return void
-		 */
-		public function render_content() {
-			?>
-			<div class="checkbox_switch">
-				<span class="customize-control-title onoffswitch_label"><?php echo esc_html( $this->label ); ?>
-					<?php if ( ! empty( $this->description ) ) : ?>
-					<i class="dashicons dashicons-editor-help" style="vertical-align: text-bottom; position: relative;">
-						<span class="mte-tooltip"><?php echo wp_kses_post( $this->description ); ?></span>
-					</i>
-					<?php endif; ?>
-				</span>
-				<div class="onoffswitch">
-					<input type="checkbox" id="<?php echo esc_attr( $this->id ); ?>"
-						   name="<?php echo esc_attr( $this->id ); ?>" class="onoffswitch-checkbox"
-						   value="<?php echo esc_attr( $this->value() ); ?>" <?php $this->link();
-							checked( $this->value() ); ?>>
-					<label class="onoffswitch-label" for="<?php echo esc_attr( $this->id ); ?>"></label>
-				</div>
-			</div>
-			<?php
-		}
+	/**
+	 * Epsilon_Control_Toggle constructor.
+	 *
+	 * @param WP_Customize_Manager $manager
+	 * @param string               $id
+	 * @param array                $args
+	 */
+	public function __construct( WP_Customize_Manager $manager, $id, array $args = array() ) {
+		parent::__construct( $manager, $id, $args );
+		$manager->register_control_type( 'Epsilon_Control_Toggle' );
 	}
-}// End if().
+
+	/**
+	 * Add custom parameters to pass to the JS via JSON.
+	 *
+	 * @since  1.2.0
+	 * @access public
+	 */
+	public function json() {
+		$json = parent::json();
+
+		$json['id']      = $this->id;
+		$json['link']    = $this->get_link();
+		$json['value']   = $this->value();
+
+		return $json;
+	}
+
+	/**
+	 * Empty, as it should.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return void
+	 */
+	public function render_content() {}
+
+	/**
+	 * @since  1.2.0
+	 * @access public
+	 */
+	public function content_template() {
+		//@formatter:off
+		?>
+		<div class="checkbox_switch">
+			<span class="customize-control-title onoffswitch_label">
+				{{{ data.label }}}
+				<# if( data.description ){ #>
+					<i class="dashicons dashicons-editor-help" style="vertical-align: text-bottom; position: relative;">
+						<span class="mte-tooltip">
+							{{{ data.description }}}
+						</span>
+					</i>
+				<# } #>
+			</span>
+			<div class="onoffswitch">
+				<input type="checkbox" id="{{{ data.id }}}" name="{{{ data.id }}}" class="onoffswitch-checkbox" value="{{{ data.value }}}" {{{ data.link }}} <# if( data.value ) { #> checked="checked" <# } #> >
+				<label class="onoffswitch-label" for="{{{ data.id }}}"></label>
+			</div>
+		</div>
+		<?php
+		//@formatter:on
+	}
+
+}
+
