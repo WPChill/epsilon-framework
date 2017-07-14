7e375a4e8cb95b00677e1c37ca87c5851138f33b
Cosmin <cristea.cosmin87@gmail.com>
2017-07-14 20:36:12 +0300

    Upsell style fixes

--- assets/js/epsilon.js
+++ assets/js/epsilon.js
@@ -2484,6 +2484,7 @@ wp.customize.controlConstructor[ 'epsilon-upsell' ] = wp.customize.Control.exten
     var control = this;
     control.container.on( 'click', '.epsilon-upsell-label', function( e ) {
       e.preventDefault();
+      jQuery( this ).toggleClass( 'opened' ).find('i').toggleClass('dashicons-arrow-down-alt2 dashicons-arrow-up-alt2');
       control.container.find( '.epsilon-upsell-container' ).slideToggle( 200 );
     } );
   }
