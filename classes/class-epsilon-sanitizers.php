a05dabd211253f1233feeec5b2b50f805c7d4ba9
Cosmin <cristea.cosmin87@gmail.com>
2017-07-25 10:08:45 +0300

    Radio buttons fix

--- classes/class-epsilon-sanitizers.php
+++ classes/class-epsilon-sanitizers.php
@@ -58,7 +58,8 @@ class Epsilon_Sanitizers {
 		global $wp_customize;
 
 		$control = $wp_customize->get_control( $setting->id );
-		if ( array_key_exists( $input, $control->choices ) ) {
+
+		if ( is_array( $control->choices ) && array_key_exists( $input, $control->choices ) ) {
 			return $input;
 		}
 
