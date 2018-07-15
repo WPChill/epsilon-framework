e777e2ac74a04fb500186c4dae45a1384a7b588b
Cristian Raiber <cristian@machothemes.com>
2018-07-15 21:57:21 +0300

    rewrote Quickie Bar in TypeScript

--- assets/vendors/epsilon-framework/quickie/quick-bar.ts
+++ assets/vendors/epsilon-framework/quickie/quick-bar.ts
@@ -1,52 +1,101 @@
-declare var require: any, wp: any;
+declare var EpsilonQuickieObj: any;
 import * as $ from 'jquery';
 
 /**
  * Epsilon Quick bar
  */
 export class EpsilonQuickieBar {
-  /**
-   * Param 1
-   * @type {string}
-   */
-  public $paramOne = '';
-  /**
-   * Param 2
-   * @type {{}}
-   */
-  public $paramTwo = {};
-  /**
-   * Param 3
-   * @type {any[]}
-   */
-  public $paramThree = [];
 
-  /**
-   * Class constructor
-   *
-   * @can have args
-   */
-  public constructor() {
-    this.$paramOne = 'Hello';
+    /**
+     * Script initiator
+     */
+    public init() {
+        this.prependEpsilonHTML();
+        this.moveResponsiveControls();
+        this.listenForClick();
+    }
 
-    this.$paramTwo = {
-      hello: 'world'
-    };
+    /**
+     * Adds body classes
+     */
+    public addBodyClass() {
+        jQuery('body').addClass('epsilon-quickie-is-visible');
+    }
 
-    this.$paramThree.push( 'Hello', 'World' );
-  }
+    /**
+     * Function that handles the EpsilonHTML rendering for the Quickie Shortcuts Bar
+     */
+    public prependEpsilonHTML() {
 
-  /**
-   * Script initiator
-   */
-  public init() {
+        if (null !== EpsilonQuickieObj) {
+            let EpsilonHTML: string = `<div class="epsilon-quickie">
 
-  }
+        <div class="epsilon-quickie-logo">
+            <img src="${EpsilonQuickieObj.logo.url}" alt="${EpsilonQuickieObj.logo.alt}">
+        </div><!--/.epsilon-quickie-logo-->
 
-  /**
-   * Adds body classes
-   */
-  public addBodyClass() {
+        <div class="epsilon-quickie-shortcuts">`;
 
-  }
+            jQuery(EpsilonQuickieObj.links).each(function (index: number, value: any) {
+                if ('' !== value) {
+                    EpsilonHTML += `<a href="#" class="epsilon-quickie-navigation" data-customizer-link="${value.link_to}" data-customizer-type="${value.link_type}">
+                        <i class="${value.icon}"></i>
+                    </a>`;
+                }
+            });
+
+            EpsilonHTML += ` </div><!--/.epsilon-quickie-shortcuts--></div>`;
+
+            // prepend the built EpsilonHTML
+            jQuery('.wp-full-overlay-sidebar').prepend(EpsilonHTML);
+
+        }
+    }
+
+    /**
+     * Function that listens for clicks on epsilon-quickie-navigation links
+     * and redirects to the corresponding section/panel/control
+     */
+    public listenForClick() {
+        const context = '.epsilon-quickie-navigation';
+
+        jQuery(context).on('click', function (event: any) {
+
+            // since they're links, prevent default
+            event.preventDefault();
+
+            let link_to: string = jQuery(this).data('customizer-link'),
+                link_type: string = jQuery(this).data('customizer-type');
+
+            if ('' !== link_to && '' !== link_type) {
+
+                /**
+                 * navigate to panel
+                 * link_type must only take values: section, control or panel
+                 */
+                wp.customize[link_type](link_to).focus();
+
+                // remove all 'quickie-link-active' CSS classes, if any
+                jQuery(context).removeClass('quickie-link-active');
+
+                // add 'quickie-link-active' on currently clicked link
+                if (jQuery(` [data-customizer-link='jQuery{link_to}']`)) {
+                    jQuery(`[data-customizer-link='jQuery{link_to}']`).addClass('quickie-link-active');
+                }
+            }
+        });
+    }
+
+    public moveResponsiveControls() {
+        const context: object = jQuery('#customize-footer-actions');
+
+        // hide the whole footer actions section
+        jQuery(context).hide();
+
+        // hide the "collapse settings" label
+        jQuery(context).find('.collapse-sidebar-label').hide();
+
+        // move the responsive controls onto the epsilon-quickie sidebar
+        jQuery(context).find('.collapse-sidebar, .devices-wrapper').appendTo('.epsilon-quickie');
+    }
 }
